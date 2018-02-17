ea418bc40e352ce29759aa3c94285bb593fc7eff
Stefan Remer <stefan.remer@gmail.com>
2018-02-17 16:14:00 +0100

    adding, deleting and order changing of items

--- src/components/Interval.js
+++ src/components/Interval.js
@@ -1,52 +1,88 @@
-import React, { Component } from 'react';
-import IntervalItem from './IntervalItem';
-import AddIntervalItem from './AddIntervalItem';
-import IntervalControls from './IntervalControls';
+import React, { Component } from 'react'
+import IntervalItem from './IntervalItem'
+import AddIntervalItem from './AddIntervalItem'
+import IntervalControls from './IntervalControls'
+import uuid from 'uuid'
+import { arrayMove } from '../utility'
 
 export default class Interval extends Component {
 
-  componentWillMount() {
-    this.setState(
-      {
-        id: 1,
-        userid: 1,
-        items: [
-          {
-            id: 0,
-            type: "Exercise",
-            name: "Burpees",
-            duration: "60"
-          },
-          {
-            id: 1,
-            type: "Break",
-            duration: "20"
-          }
-        ]
-      }
-    );
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: 1,
+      userid: 1,
+      items: [
+        {
+          id: uuid(),
+          active: true,
+          type: "Exercise",
+          name: "Burpees",
+          duration: "60"
+        },
+        {
+          id: uuid(),
+          active: false,
+          type: "Break",
+          duration: "20"
+        }
+      ]
+    }
+  }
+
+  componentDidMount() {
+    console.log(this.state)
+  }
+
+  componentDidUpdate() {
+    console.log(this.state)
   }
 
   handleAddItem = (item) => {
     const items = this.state.items.slice();
-    item.id = items.length
-    items.push(item)
+    const newItem = Object.assign({}, item)
+    newItem.id = uuid()
+    items.push(newItem)
     this.setState({
       items: items
     })
-    console.log(this.state)
   }
 
   handleDeleteItem = (e, item) => {
-    console.log(e)
-    console.log(item)
+    console.log(this.state)
+    const items = this.state.items.slice();
+    const filtered = items.filter(e => e !== item)
+    this.setState({
+      items: filtered
+    })
     e.preventDefault()
+  }
+
+  handleUpItem = (e, item) => {
+    const oldIndex = this.state.items.findIndex(x => x.id === item.id)
+    const newIndex = oldIndex > 0 ? oldIndex - 1 : oldIndex
+    const newItems = arrayMove(this.state.items, oldIndex, newIndex)
+    this.setState({ items: newItems })
+  }
+
+  handleDownItem = (e, item) => {
+    const items = this.state.items
+    const oldIndex = items.findIndex(x => x.id === item.id)
+    const newIndex = oldIndex < items.length - 1 ? oldIndex + 1 : oldIndex
+    const newItems = arrayMove(items, oldIndex, newIndex)
+    this.setState({ items: newItems })
   }
 
   render() {
 
     const listItems = this.state.items.map(item =>
-      <IntervalItem key={item.id} item={item} deleteItem={(event) => { this.handleDeleteItem(event, item) }} />
+      <IntervalItem
+        key={item.id}
+        item={item}
+        deleteItem={(event) => { this.handleDeleteItem(event, item) }}
+        upItem={(event) => { this.handleUpItem(event, item) }}
+        downItem={(event) => { this.handleDownItem(event, item) }}
+      />
     )
 
     return (
@@ -58,7 +94,7 @@ export default class Interval extends Component {
             <IntervalControls />
           </nav>
         </div>
-        <AddIntervalItem addItem={this.handleAddItem} />
+        <AddIntervalItem addItem={(item) => { this.handleAddItem(item) }} />
       </div >
     );
   }
--- src/components/IntervalItem.js
+++ src/components/IntervalItem.js
@@ -7,6 +7,16 @@ class IntervalItem extends Component {
         return (
             <div className="panel-block" style={{ display: 'block' }}>
                 <div className="columns">
+
+                    <div className="column">
+                        <span className="icon" onClick={this.props.upItem}>
+                            <i className="fas fa-angle-up" />
+                        </span>
+                        <span className="icon" onClick={this.props.downItem}>
+                            <i className="fas fa-angle-down" />
+                        </span>
+                    </div>
+
                     <div className="column is-one-fifths">
                         {item.name ? item.name : "Break"}
                     </div>
